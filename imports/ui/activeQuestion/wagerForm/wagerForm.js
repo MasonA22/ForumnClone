f53b7b7ac2c73b7043afd2d3ad22b71a94693c4d
programthis <kylebachan@gmail.com>
2016-09-08 11:48:58 -0400

    Added input range package back into app

--- imports/ui/activeQuestion/wagerForm/wagerForm.js
+++ imports/ui/activeQuestion/wagerForm/wagerForm.js
@@ -4,21 +4,21 @@ import { Template } from "meteor/templating";
 import "./wagerForm.html";
 
 Template.wagerForm.onRendered(function(){
-	// var slider = document.getElementById('wageRangeSlider');
-	// var score = Meteor.user().profile.score;
-	// if (score > 0){
-	// 	$("#wageRangeSlider").noUiSlider({
-	// 		start: 0,
-	// 		connect: "lower",
-	// 		range: {
-	// 			'min': 0,
-	// 		    'max': score
-	// 		}
-	// 	}).on('slide', function (ev, val) {
-	// 		val = Math.floor(val);
-	// 		var newScore = score - val;
-	// 		$(".wagerTotalLeft").text(newScore);
-	// 		$(".userWager").val(val);
-	// 	});
-	// }
+	var slider = document.getElementById('wageRangeSlider');
+	var score = Meteor.user().profile.score;
+	if (score > 0){
+		$("#wageRangeSlider").noUiSlider({
+			start: 0,
+			connect: "lower",
+			range: {
+				'min': 0,
+			    'max': score
+			}
+		}).on('slide', function (ev, val) {
+			val = Math.floor(val);
+			var newScore = score - val;
+			$(".wagerTotalLeft").text(newScore);
+			$(".userWager").val(val);
+		});
+	}
 });
