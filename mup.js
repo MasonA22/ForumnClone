1c20cd43b200e48651715aa4ab9f0fe83cfd4b20
programthis <kylebachan@gmail.com>
2016-09-19 10:31:09 -0400

    Updated mup config

--- mup.js
+++ mup.js
@@ -1,37 +1,37 @@
 module.exports = {
-  servers: {
-    one: {
-      host: '138.197.138.68',
-      username: 'root'
-      // pem:
-      // password:
-      // or leave blank for authenticate from ssh-agent
-    }
-  },
-
-  meteor: {
-    name: 'app',
-    path: '../app',
     servers: {
-      one: {}
-    },
-    buildOptions: {
-      serverOnly: true,
-    },
-    env: {
-      ROOT_URL: 'app.com',
-      MONGO_URL: 'mongodb://localhost/meteor'
+        one: {
+            host: '138.197.138.68',
+            username: 'root',
+            // pem:
+            password: "SHaolinsoccer10"
+            // or leave blank for authenticate from ssh-agent
+        }
     },
 
-    //dockerImage: 'kadirahq/meteord'
-    deployCheckWaitTime: 60
-  },
+    meteor: {
+        name: 'ForumLive',
+        path: '../app',
+        servers: {
+            one: {}
+        },
+        buildOptions: {
+            serverOnly: true,
+        },
+        env: {
+            ROOT_URL: 'http://138.197.138.68/',
+            MONGO_URL: 'mongodb://localhost/meteor'
+        },
 
-  mongo: {
-    oplog: true,
-    port: 27017,
-    servers: {
-      one: {},
+        //dockerImage: 'kadirahq/meteord'
+        deployCheckWaitTime: 60
     },
-  },
+
+    mongo: {
+        oplog: true,
+        port: 27017,
+        servers: {
+            one: {},
+        },      
+    },
 };
