738c4de60a82cc404ca6672f48431e9086a39c2a
programthis <kylebachan@gmail.com>
2016-09-19 13:05:35 -0400

    Working to remove tap highlight on mobile

--- mup.js
+++ mup.js
@@ -26,7 +26,6 @@ module.exports = {
             MONGO_URL: 'mongodb://localhost/meteor'
         },
         dockerImage: 'abernix/meteord:base',
-
         //dockerImage: 'kadirahq/meteord'
         deployCheckWaitTime: 60
     },
