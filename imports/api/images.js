092230ec62c9cb0c8fa7e33907be7138fb167fa0
programthis <kylebachan@gmail.com>
2016-09-20 15:14:24 -0400

    Fixed delete image

--- imports/api/images.js
+++ imports/api/images.js
@@ -2,7 +2,6 @@ import { FilesCollection } from 'meteor/ostrio:files';
 
 export const Images = new FilesCollection({
     collectionName: 'Images',
-    allowClientCode: false, // Disallow remove files from Client
     onBeforeUpload: function (file) {
         // Allow upload files under 10MB, and only in png/jpg/jpeg formats
         if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.extension)) {
