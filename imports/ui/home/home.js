d330513dbbcfa094ebdd83493a25d27de0b1dc92
programthis <kylebachan@gmail.com>
2016-10-03 14:09:56 -0400

    Changing home page to match the new admin panel UI

--- imports/ui/home/home.js
+++ imports/ui/home/home.js
@@ -7,3 +7,38 @@ import "../rooms/rooms.js";
 import "../badges/badges.js";
 import "../feedbackTypes/feedbackTypes.js";
 import "../images/images.js";
+
+Template.home.onCreated(function(){
+	this.state = new ReactiveDict();
+	const instance = Template.instance();
+	instance.state.set("questions", false);
+	instance.state.set("rooms", false);
+	instance.state.set("badges", false);
+	instance.state.set("feedbackTypes", false);
+});
+
+Template.home.helpers({
+	showAdminSection: function(sectionType) {
+		const instance = Template.instance();
+		if (instance.state.get(sectionType)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	} 
+});
+
+Template.home.events({
+	"click .adminManagementHeader": function(evt, template) {
+		evt.preventDefault();
+		let adminOption = $(evt.target).closest(".adminManagementContainer").attr("adminOption");
+		console.log(adminOption);
+		if (template.state.get(adminOption)){
+			template.state.set(adminOption, false);
+		}
+		else{
+			template.state.set(adminOption, true);
+		}
+	}
+});
